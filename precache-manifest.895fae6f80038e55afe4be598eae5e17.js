c6c0b126d7cb60ef412d6822070f58d4bb387a5b
Vadim Sirenko <vadim7ko@gmail.com>
2019-10-07 21:35:06 +0300

    Updates

---
+++ precache-manifest.895fae6f80038e55afe4be598eae5e17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ccd06ac2c1670a312c22d61abe8bba9",
+    "url": "/library/index.html"
+  },
+  {
+    "revision": "bfe47e27770ec367b4ef",
+    "url": "/library/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "eba38c2cce2d0ac02416",
+    "url": "/library/static/js/2.dff070ed.chunk.js"
+  },
+  {
+    "revision": "bfe47e27770ec367b4ef",
+    "url": "/library/static/js/main.c4fee03c.chunk.js"
+  },
+  {
+    "revision": "4a2850cf760ac4297ad5",
+    "url": "/library/static/js/runtime-main.7d816809.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/library/static/media/logo.25bf045c.svg"
+  }
+]);
